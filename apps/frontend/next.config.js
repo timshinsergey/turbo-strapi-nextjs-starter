3341927c323b05ebe643a58783ee9e16ecbacdcb
Sergey <33506866+timshinsergey@users.noreply.github.com>
2022-09-12 13:08:56 +0400

    create docker for frontend

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,3 +1,9 @@
+const path = require("path");
+
 module.exports = {
+  output: "standalone",
   reactStrictMode: true,
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, "../../"),
+  },
 };
