8de5aeb9701275a69dbee3836893ab6be7e5c37b
Sergey <33506866+timshinsergey@users.noreply.github.com>
2022-11-02 23:59:22 +0500

    configure docker

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,4 +1,10 @@
+const path = require('path')
+
 module.exports = {
 	output: 'standalone',
 	reactStrictMode: true,
+	experimental: {
+		outputFileTracingRoot: path.join(__dirname, '../../'),
+		transpilePackages: ['ui'],
+	},
 }
